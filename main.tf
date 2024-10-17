d41c1b775c78c8f22dbc85af1a143f1a5f7b309d
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-10-17 09:05:14 -0600

    Create resource group

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=4.5.0"
+    }
+  }
+}
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
+  subscription_id = "7021a6a2-42bd-4f99-b2bb-3e2e89b8749e"
 }
 
 resource "azurerm_resource_group" "rg" {
