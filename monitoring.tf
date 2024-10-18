a22841e991a0d9dfcf0e006553e8fcc57e3ba59c
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-10-17 21:57:27 -0600

    Add insights monitoring

---
+++ monitoring.tf
@@ -0,0 +1,14 @@
+resource "azurerm_application_insights" "app_insights" {
+  name                = "appinsights-${var.project}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.app_insights.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.app_insights.app_id
+}
